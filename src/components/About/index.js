c90207d93a1dd3c521d37300c9dfef8a174ec8ae
Will Darkins <willdarkins@gmail.com>
2021-12-12 21:47:22 -0800

    attributed className to section tage

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function About () {
     return (
-        <section>
+        <section className='my-5'>
                 <h1 id='about'>
                     Who am I?
                 </h1>
