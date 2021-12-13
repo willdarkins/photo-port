3ecb1b91782517185f98e4011a024d4c397aa522
Will Darkins <willdarkins@gmail.com>
2021-12-12 23:22:03 -0800

    ran Nav test and it works

--- src/components/Nav/_tests_/index.test.js
+++ src/components/Nav/_tests_/index.test.js
@@ -2,3 +2,11 @@ import React from "react";
 import { render, cleanup } from "@testing-library/react";
 import '@testing-library/jest-dom/extend-expect';
 import Nav from '..';
+
+afterEach(cleanup);
+
+describe('Nav component', () => {
+    it('renders', () => {
+        render(<Nav />);
+      });
+})
