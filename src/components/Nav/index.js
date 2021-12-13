9a612c333411b08d024bf18c53f5553f95126944
Will Darkins <willdarkins@gmail.com>
2021-12-12 22:05:34 -0800

    added styles and other attributes to nav index

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -3,7 +3,23 @@ import React from "react";
 function Nav() {
     return (
         <header>
-
+            <h2>
+                <a href="/">
+                    <span role="img" aria-label="camera">📸</span> Oh Snap!
+                </a>
+            </h2>
+            <nav>
+                <ul className="flex-row">
+                    <li className="mx-2">
+                        <a href="#about">
+                            About Me
+                        </a>
+                    </li>
+                    <li>
+                        <span>Contact</span>
+                    </li>
+                </ul>
+            </nav>
         </header>
     )
 }
