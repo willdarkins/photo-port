aeb86cbb02c94fcb57565eaa66f7d3d782826657
Will Darkins <willdarkins@gmail.com>
2021-12-12 22:48:33 -0800

    imported all testing libraries into index.test.js and wrote first test in describe function

--- src/components/About/_tests_/index.test.js
+++ src/components/About/_tests_/index.test.js
@@ -1,3 +1,15 @@
 import React from "react";
-
+//retrieve some functions from the React Testing Library
 import { render, cleanup } from '@testing-library/react'
+//jest-dom offers access to custom matchers that are used to test DOM elements
+import '@testing-library/jest-dom/extend-expect'
+import About from '..';
+//this ensures that after each test, we won't have any leftover memory data that could give us false results
+afterEach(cleanup);
+// the describe function to declare the component we're testing
+describe('About component', () => {
+    //the first test will be the baseline to verify that the component is rendering
+    it('renders', () => {
+        render(<About />);
+      });
+})
