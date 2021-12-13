1c4bd8b9d9aae1f16ef3e2e19c9fff5891637849
Will Darkins <willdarkins@gmail.com>
2021-12-13 00:04:43 -0800

    wrote testing to see availability of emoji in h2 header

--- src/components/Nav/_tests_/index.test.js
+++ src/components/Nav/_tests_/index.test.js
@@ -4,17 +4,27 @@ import '@testing-library/jest-dom/extend-expect';
 import Nav from '..';
 
 afterEach(cleanup);
-
+//test Nav component
 describe('Nav component', () => {
     // baseline test
     it('renders', () => {
-      render(<Nav />);
+        render(<Nav />);
     });
-  
+
     // snapshot test
     it('matches snapshot', () => {
-      const { asFragment } = render(<Nav />);
-      // assert value comparison
-      expect(asFragment()).toMatchSnapshot();
+        const { asFragment } = render(<Nav />);
+        // assert value comparison
+        expect(asFragment()).toMatchSnapshot();
     });
-  })
+})
+//test visibility of camera emoji in Nav
+describe('emoji is visible', () => {
+    it('inserts emoji into the h2', () => {
+    // Arrange
+    const { getByLabelText } = render(<Nav />);
+    // Assert
+    //getByLabelText method and query by the aria-label value
+    expect(getByLabelText('camera')).toHaveTextContent('📸');
+    })
+  })  
