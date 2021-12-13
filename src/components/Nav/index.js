84330ebb6263a9b4a8850d586370b860ad910b5c
Will Darkins <willdarkins@gmail.com>
2021-12-12 22:06:44 -0800

    added categories array in order to map over options

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,6 +1,18 @@
 import React from "react";
 
 function Nav() {
+    const categories = [
+        {
+            name: "commercial",
+            description:"Photos of grocery stores, food trucks, and other commercial projects",
+        },
+        { name: "portraits", description: "Portraits of people in my life" },
+        { name: "food", description: "Delicious delicacies" },
+        {
+            name: "landscape",
+            description: "Fields, farmhouses, waterfalls, and the beauty of nature",
+        },
+    ];
     return (
         <header>
             <h2>
