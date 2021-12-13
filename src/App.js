7e68c65c45c21208c6d24d83392656bf51fe4f50
Will Darkins <willdarkins@gmail.com>
2021-12-12 21:39:51 -0800

    minor changes to app.js with test to make sure React server was working... it was

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
+//this App.js file is the center of the application
+import About from './components/About';
 import React from 'react';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <main>
+        <About />
+      </main>
     </div>
   );
 }
