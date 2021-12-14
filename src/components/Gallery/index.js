67fe414e63285ff5fb8b6b779e3d1a2427c9d69c
Will Darkins <willdarkins@gmail.com>
2021-12-13 21:50:27 -0800

    added currentCategory object to Gallery component

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 
 function Gallery () {
+    currentCategory = {
+        name: "commercial",
+        description: "Photos of grocery stores, food trucks, and other commercial projects"
+    }
     return (
         <section>
             <h1>NAME</h1>
