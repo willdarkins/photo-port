0f9eecbb0e585b76c8d7eb93c86cb82a31734b67
Will Darkins <willdarkins@gmail.com>
2021-12-13 21:46:57 -0800

    created Gallery component and corresponding directories to hold tests... also imported into App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import './assets/index.css';
 import About from './components/About';
 import Nav from './components/Nav';
+import Gallery from './components/Gallery';
 
 function App() {
   return (
     <div>
       <Nav />
       <main>
+        <Gallery />
         <About />
       </main>
     </div>
