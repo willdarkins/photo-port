252eb7b64f3e36542f52031c0036af8ca80beb8f
Will Darkins <willdarkins@gmail.com>
2021-12-15 22:14:09 -0800

    added setFormState to ContactForm component

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,22 +1,25 @@
 import React, {useState} from "react";
 
 function ContactForm () {
-    const [formState, setFormState] = state()
+    //we want to clear the input fields on the component loading - thus empty strings
+    const [formState, setFormState] = state({name: '', email: '', message: ''})
+    // destructure the formState object into its named properties, name, email, and message.
+    const { name, email, message } = formState;
     return (
     <section>
         <h1>Contact Me</h1>
         <form id="contact-form">
             <div>
                 <label htmlFor="name">Name:</label>
-                <input type="text" name = "name" />
+                <input type="text" name = "name" defaultValue={name}/>
             </div>
             <div>
                 <label htmlFor="email">Email Address:</label>
-                <input type = "email" name = "email" />
+                <input type = "email" name = "email" defaultValue={email}/>
             </div>
             <div>
                 <label htmlFor="message">Message:</label>
-                <textarea name = "message" rows = "5" />
+                <textarea name = "message" rows = "5" defaultValue={message}/>
             </div>
             <button type="submit">Submit</button>
         </form>
