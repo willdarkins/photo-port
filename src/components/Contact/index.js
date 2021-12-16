fcccc835bc6bd356e6fd1f248d4be0dc7a436ce0
Will Darkins <willdarkins@gmail.com>
2021-12-15 23:43:23 -0800

    added error handling for email input on form for Contact component and ContactForm function

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,8 +1,10 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import { validateEmail } from "../../utils/helpers";
 
-function ContactForm () {
+function ContactForm() {
+    const [errorMessage, setErrorMessage] = useState('');
     //we want to clear the input fields on the component loading - thus empty strings
-    const [formState, setFormState] = useState({name: '', email: '', message: ''})
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' })
     // destructure the formState object into its named properties, name, email, and message.
     const { name, email, message } = formState;
     // using the setFormState function to update the formState value for the name property
@@ -12,35 +14,52 @@ function ContactForm () {
     // name property of target in the second expression actually refers to the name attribute of the form element
     //matches the property names of formState (name, email, and message)
     function handleChange(e) {
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            console.log(isValid);
+            // isValid conditional statement
+            if (!isValid) {
+                setErrorMessage('Your email is invalid.');
+            } else {
+                if (!e.target.value.length) {
+                    setErrorMessage(`${e.target.name} is required.`);
+                } else {
+                    setErrorMessage('');
+                }
+            }
+        }
         // setFormState({...formState, name: e.target.value})
-        setFormState({...formState, [e.target.name]: e.target.value})
+        //conditional so that the state only updates if the form data has passed the quality tests
+        if (!errorMessage) {
+            setFormState({ ...formState, [e.target.name]: e.target.value });
+        }
     }
     // function that'll handle the submission of the form data
     function handleSubmit(e) {
         e.preventDefault();
         console.log(formState);
-      }
+    }
     return (
-    <section>
-        <h1>Contact Me</h1>
-        <form id="contact-form" onSubmit={handleSubmit}>
-            <div>
-                <label htmlFor="name">Name:</label>
-                {/* onChange is an event listener */}
-                <input type="text" name = "name" defaultValue={name} onChange={handleChange}/>
-            </div>
-            <div>
-                <label htmlFor="email">Email Address:</label>
-                <input type = "email" name = "email" defaultValue={email} onChange={handleChange}/>
-            </div>
-            <div>
-                <label htmlFor="message">Message:</label>
-                <textarea name = "message" rows = "5" defaultValue={message} onChange={handleChange}/>
-            </div>
-            <button type="submit">Submit</button>
-        </form>
-    </section>
-)
+        <section>
+            <h1>Contact Me</h1>
+            <form id="contact-form" onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="name">Name:</label>
+                    {/* onChange is an event listener */}
+                    <input type="text" name="name" defaultValue={name} onChange={handleChange} />
+                </div>
+                <div>
+                    <label htmlFor="email">Email Address:</label>
+                    <input type="email" name="email" defaultValue={email} onChange={handleChange} />
+                </div>
+                <div>
+                    <label htmlFor="message">Message:</label>
+                    <textarea name="message" rows="5" defaultValue={message} onChange={handleChange} />
+                </div>
+                <button type="submit">Submit</button>
+            </form>
+        </section>
+    )
 }
 
 export default ContactForm
