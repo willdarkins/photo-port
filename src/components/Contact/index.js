c093053359e1eeaa6c018f04c9bb10bda8b9255f
Will Darkins <willdarkins@gmail.com>
2021-12-15 21:24:38 -0800

    added form shell to index.js for Contact component

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function ContactForm () {
-
+    <section>
+        <h1>Contact Me</h1>
+        <form id="contact-form">
+            
+        </form>
+    </section>
 }
 
 export default ContactForm
