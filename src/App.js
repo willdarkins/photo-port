fc235498235ee47b7bdc3972ecb6c215d79c031f
Will Darkins <willdarkins@gmail.com>
2021-12-15 21:15:31 -0800

    had to reformat a few things with my styles... all good now

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './assets/index.css'
 import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from './components/Gallery';
