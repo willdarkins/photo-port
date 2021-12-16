eb0978315a53236dd704b2ab331ec3e47fc70519
Will Darkins <willdarkins@gmail.com>
2021-12-15 21:30:35 -0800

    added labels and text areas for About component

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -4,7 +4,18 @@ function ContactForm () {
     <section>
         <h1>Contact Me</h1>
         <form id="contact-form">
-            
+            <div>
+                <label htmlFor="name">Name:</label>
+                <input type="text" name = "name" />
+            </div>
+            <div>
+                <label htmlFor="email">Email Address:</label>
+                <input type = "email" name = "email" />
+            </div>
+            <div>
+                <label htmlFor="message">Message:</label>
+                <textarea name = "message" rows = "5" />
+            </div>
         </form>
     </section>
 }
